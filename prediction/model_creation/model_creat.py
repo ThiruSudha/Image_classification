9e7fb14e59e173ddda945a55c7cc272637709a24
Thirumoorthi1998 <83215532+Thirumoorthi1998@users.noreply.github.com>
2021-12-23 18:10:39 +0530

    Update model_creat.py

--- prediction/model_creation/model_creat.py
+++ prediction/model_creation/model_creat.py
@@ -1,1 +1,59 @@
 
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,\
+     Dropout,Flatten,Dense,Activation,\
+     BatchNormalization
+from tensorflow.keras import layers
+import random
+import os
+import tensorflow as tf
+img_height,img_width=180,180
+batch_size=32
+data_dir ="stroke_data"
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+class_names = train_ds.class_names
+print(class_names)
+num_classes = 2
+
+model = Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes,activation='softmax')
+])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+epochs=10
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+model.save("perfect_models/Stroke_best.h5")
+
+# def predict_image(img):
+#   img_4d=img.reshape(-1,180,180,3)
+#   prediction=model.predict(img_4d)[0]
+#   return {class_names[i]: float(prediction[i]) for i in range(5)}
